49b411d8611597f3d1c08fac4939d7dc523b7b66
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2017-10-29 19:45:52 +0300

    Use local images for speakers

--- DotNetRu.DataStore.Audit/Entities/SpeakerEntity.cs
+++ DotNetRu.DataStore.Audit/Entities/SpeakerEntity.cs
@@ -2,19 +2,25 @@
 {
     using System.Xml.Serialization;
 
-    [XmlType(AnonymousType = true)]
-    [XmlRoot(Namespace = "", IsNullable = false)]
-    public class Speaker
+    [XmlType("Speaker")]
+    public class SpeakerEntity
     {
         public string Id { get; set; }
+
         public string Name { get; set; }
+
         public string CompanyName { get; set; }
+
         public string CompanyUrl { get; set; }
+
         public string Description { get; set; }
 
         public string BlogUrl { get; set; }
+
         public string TwitterUrl { get; set; }
+
         public string HabrUrl { get; set; }
+
         public string ContactsUrl { get; set; }
     }
 }
