ed2bc9020aabb9965a6b23e7d479f629e8a568a3
Sergey <piteryo7@gmail.com>
2017-11-06 01:20:47 +0300

    First steps to make localization

---
+++ DotNetRu.Clients.UI/TranslateExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinEvolve.Clients.Portable.Interfaces;
+
+namespace XamarinEvolve.Clients.UI
+{
+    [ContentProperty("Text")]
+    class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci;
+        const string ResourceId = "XamarinEvolve.Clients.UI.AppResources.AppResources";
+
+        private static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(ResourceId
+            , typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+        public TranslateExtension()
+        {
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            }
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                    "Text");
+#else
+                translation = Text; // returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
