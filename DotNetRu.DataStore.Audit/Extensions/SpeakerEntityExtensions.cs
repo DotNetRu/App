5ff285663b4505fe4f976a9d7a6a34ae5ae4fe0f
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2017-11-26 14:25:10 +0300

    Fixed displaying of speaker photos and friends

--- DotNetRu.DataStore.Audit/Extensions/SpeakerEntityExtensions.cs
+++ DotNetRu.DataStore.Audit/Extensions/SpeakerEntityExtensions.cs
@@ -18,21 +18,30 @@
             // speaker.Id
             // }/avatar.small.jpg",
 
-            // Path to an embedded resource should have '_' instead of '-' 
-            string imagePath = "DotNetRu.DataStore.Audit.Storage.speakers." + speaker.Id.Replace("-", "_");
+            // Display names of embedded resources
+            //var assembly = typeof(SpeakerEntity).GetTypeInfo().Assembly;
+            //foreach (var res in assembly.GetManifestResourceNames())
+            //{
+            //    System.Diagnostics.Debug.WriteLine(">>> " + res);
+            //}
+
+            string imagePathPrefix = "DotNetRu.DataStore.Audit.Storage.speakers." + speaker.Id;
+
+            string avatarPath = imagePathPrefix + ".avatar.jpg";
+            string smallAvatarPath = imagePathPrefix + ".avatar.small.jpg";
+
             return new SpeakerModel
                        {
                            Id = speaker.Id,
                            FirstName = speaker.Name,
                            LastName = string.Empty,
-                           PhotoImage = ImageSource.FromResource(imagePath + ".avatar.jpg"),
-                           AvatarImage = ImageSource.FromResource(imagePath + ".avatar.small.jpg"),
+                           PhotoImage = ImageSource.FromResource(avatarPath),
+                           AvatarImage = ImageSource.FromResource(smallAvatarPath),
                            CompanyName = speaker.CompanyName,
                            CompanyWebsiteUrl = speaker.CompanyUrl,
                            TwitterUrl = speaker.TwitterUrl,
                            BlogUrl = speaker.BlogUrl,
                            Biography = speaker.Description
-                           // Talks = 
                        };
         }
     }
