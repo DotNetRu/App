ed5f9c776e406d811f9438c8c0516646a15c9a43
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2018-01-07 16:42:09 +0300

    About page UI improvements: local images, language changing and more

---
+++ DotNetRu.DataStore.Audit/Services/LogoService.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetRu.DataStore.Audit.Services
+{
+    using Xamarin.Forms;
+
+    public class LogoService
+    {
+        private static string logoPath = "DotNetRu.DataStore.Audit.Storage.logos.";
+
+        public static ImageSource DotNetRuLogo => ImageSource.FromResource(logoPath + "DotNetRu.png");
+
+        public static ImageSource SpbDotNetLogo => ImageSource.FromResource(logoPath + "SpbDotNet.png");
+
+        public static ImageSource EkbDotNetLogo => ImageSource.FromResource(logoPath + "EkbDotNet.png");
+
+        public static ImageSource KryDotNetLogo => ImageSource.FromResource(logoPath + "KryDotNet.png");
+
+        public static ImageSource MskDotNetLogo => ImageSource.FromResource(logoPath + "MskDotNet.png");
+
+        public static ImageSource SarDotNetLogo => ImageSource.FromResource(logoPath + "SarDotNet.png");
+    }
+}
--- DotNetRu.Utils/Helpers/AboutThisApp.cs
+++ DotNetRu.Utils/Helpers/AboutThisApp.cs
@@ -1,8 +1,5 @@
 ﻿namespace DotNetRu.Utils.Helpers
 {
-    /// <summary>
-    /// The about this app.
-    /// </summary>
     public static class AboutThisApp
     {
         public const string AppLinkProtocol = "dotnetru";
@@ -14,17 +11,12 @@
         public const string CompanyName = "DotNetRu";
 
         public const string Developer = "DotNetRu Team";
+
         public const string DotNetRuLink = "https://vk.com/DotNetRu";
         public const string SpbLink = "https://vk.com/SpbDotNet";
         public const string SaratovLink = "https://vk.com/SarDotNet";
         public const string KrasnoyarskLink = "https://vk.com/KryDotNet";
         public const string MoscowLink = "https://vk.com/MskDotNet";
-        public const string MoscowLogo = "https://pp.userapi.com/c841037/v841037572/2a77a/bPbj5VTogx8.jpg";
-        public const string KrasnoyarskLogo = "https://pp.userapi.com/c840234/v840234663/4b2cc/OQj9smuW9xg.jpg";
-        public const string SaratovLogo = "https://pp.userapi.com/c836223/v836223653/43add/XKJkgAvIqH4.jpg";
-        public const string SpbLogo = "https://pp.userapi.com/c840730/v840730906/31b0e/D7eL1rLAp3w.jpg";
-        public const string DotNetRuLogo = "https://pp.userapi.com/c639730/v639730011/5d57a/8uyG_Y7ZOZs.jpg";
-
 
         public const string DeveloperWebsite = "http://dotnet.ru";
 
@@ -40,13 +32,11 @@
         // TODO: use the domain name of the site you want to integrate AppLinks with
         public const string AppLinksBaseDomain = "TODO";
 
-        public const string SessionsSiteSubdirectory = "Sessies";
+        public const string SessionsSiteSubdirectory = "Sessions";
 
         public const string SpeakersSiteSubdirectory = "Speakers";
 
-        public const string SponsorsSiteSubdirectory = "Sponsors";
-
-        public const string Copyright = "Copyright 2017 - DotNetRu";
+        public const string Copyright = "Copyright 2018 - DotNetRu";
 
         public const string Credits =
             "The DotNetRu mobile app were handcrafted by DotNetRu, based on the great work done by Xamarin.\n\n"
