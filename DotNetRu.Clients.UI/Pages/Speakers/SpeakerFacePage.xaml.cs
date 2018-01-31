af25445a8cd3c92a9ea3637910a816c51ee13210
Vlad Bilyk <vlad.bilyk@gmail.com>
2018-02-01 00:23:07 +0300

    Full screen image of the speaker is shown by tap on the speaker page (#48)

---
+++ DotNetRu.Clients.UI/Pages/Speakers/SpeakerFacePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using DotNetRu.Clients.Portable.Model;
+using Xamarin.Forms;
+
+namespace DotNetRu.Clients.UI.Pages.Speakers
+{
+    public partial class SpeakerFacePage
+    {
+        public SpeakerFacePage(ImageSource image)
+        {
+            InitializeComponent();
+            faceImage.Source = image;
+        }
+
+        public override AppPage PageType => AppPage.SpeakerFace;
+    }
+}
