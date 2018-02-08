eb4ab4578a7087ee35590b0215699fb754906b5b
Pavel <pavel.fedotovsky@outlook.com>
2018-02-08 23:37:55 +0300

    UWP: more fixes

--- DotNetRu.UWP/MainPage.xaml.cs
+++ DotNetRu.UWP/MainPage.xaml.cs
@@ -1,25 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.System.Profile;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace XamarinEvolve.UWP
+namespace DotNetRu.UWP
 {
+    using System;
+    using System.Diagnostics;
+
+    using Windows.Foundation;
+    using Windows.System.Profile;
+    using Windows.UI.ViewManagement;
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
