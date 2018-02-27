ab507f66a7e5585abe8651f4ca4a4e3cf7d1ddf3
Pavel <pavel.fedotovsky@outlook.com>
2018-02-27 22:33:45 +0300

    Removed App Analytics from Debug builds

--- DotNetRu.Clients.UI/App.xaml.cs
+++ DotNetRu.Clients.UI/App.xaml.cs
@@ -50,10 +50,12 @@ namespace DotNetRu.Clients.UI
 
             this.InitializeComponent();
 
+#if RELEASE
             AppCenter.Start(
                 "ios=1e7f311f-1055-4ec9-8b00-0302015ab8ae;android=6f9a7703-8ca4-477e-9558-7e095f7d20aa;",
                 typeof(Analytics),
                 typeof(Crashes));
+#endif
 
             this.MainPage = new BottomTabbedPage();
         }
