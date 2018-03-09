27087b1073184e63c239e4b42117e039b00a531a
Pavel <pavel.fedotovsky@outlook.com>
2018-03-09 17:30:17 +0300

    Added friend logos

--- DotNetRu.DataStore.Audit/Extensions/FriendExtensions.cs
+++ DotNetRu.DataStore.Audit/Extensions/FriendExtensions.cs
@@ -1,14 +1,20 @@
 ﻿namespace DotNetRu.DataStore.Audit.Extensions
 {
+    using System.IO;
     using System.Linq;
 
     using DotNetRu.DataStore.Audit.Models;
     using DotNetRu.DataStore.Audit.RealmModels;
 
+    using Xamarin.Forms;
+
     public static class FriendExtensions
     {
         public static FriendModel ToModel(this Friend friend)
         {
+            var logoSmallMemoryStream = new MemoryStream(friend.LogoSmall);
+            var logoMemoryStream = new MemoryStream(friend.Logo);
+
             return new FriendModel
                        {
                            Id = friend.Id,
@@ -16,7 +22,9 @@
                            Name = friend.Name,
                            WebsiteUrl = friend.Url,
                            Meetups = friend.Meetups.ToList().Select(x => x.ToModel())
-                               .OrderBy(x => x.StartTime)
+                               .OrderBy(x => x.StartTime),
+                           LogoSmallImage = ImageSource.FromStream(() => logoSmallMemoryStream),
+                           LogoImage = ImageSource.FromStream(() => logoMemoryStream)
                        };
         }
     }
