8e6dbeca505bb9c20bdebb3e0aa63ff75ea4084a
Pavel <pavel.fedotovsky@outlook.com>
2018-04-07 23:33:11 +0300

    Push Notifications improvemtns: enhanced logging, background update (not working yet)

---
+++ DotNetRu.Android/DotNetRuPushReceiver.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNetRu.Droid
+{
+    using Android.App;
+    using Android.Content;
+    using Android.Runtime;
+    using Android.Util;
+
+    using DotNetRu.DataStore.Audit.Services;
+
+    [Preserve]
+    [BroadcastReceiver(Permission = "com.google.android.c2dm.permission.SEND")]
+    [IntentFilter(new[] { "com.google.android.c2dm.intent.RECEIVE" },
+        Categories = new[] { "${applicationId}" })]
+    public class DotNetRuPushReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Log.Debug("Push", "Push Notification receviced!");
+
+            UpdateService.UpdateAudit();
+        }
+    }
+}
