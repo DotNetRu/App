5327e4fe7128cf9172815005e718c6102ce39950
Pavel <pavel.fedotovsky@outlook.com>
2018-04-10 00:17:38 +0300

    Added Firebase SDK

---
+++ DotNetRu.Android/DotNetRuFirebaseIIDService.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNetRu.Droid
+{
+    using Android.App;
+    using Android.Util;
+
+    using Firebase.Iid;
+
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class DotNetRuFirebaseIIDService : FirebaseInstanceIdService
+    {
+        private const string Tag = "AuditUpdate";
+
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(Tag, "Refreshed token: " + refreshedToken);
+            this.SendRegistrationToServer(refreshedToken);
+        }
+
+        private void SendRegistrationToServer(string token)
+        {
+            // Add custom implementation, as needed.
+        }
+    }
+}
