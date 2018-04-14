376ca2764aa1add0f71a999422429b89db921a8b
Pavel <pavel.fedotovsky@outlook.com>
2018-04-14 22:57:08 +0300

    [Android] Added Firebase specific notification handler

---
+++ DotNetRu.Android/DotNetRuFirebaseMessagingService.cs
@@ -0,0 +1,65 @@
+﻿namespace DotNetRu.Droid
+{
+    using System.Threading.Tasks;
+
+    using Android.App;
+    using Android.Content;
+    using Android.Util;
+
+    using DotNetRu.DataStore.Audit.Services;
+    using DotNetRu.Droid.Helpers;
+
+    using Firebase.Messaging;
+
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class DotNetRuFirebaseMessagingService : FirebaseMessagingService
+    {
+        public const string AuditUpdateChannel = "com.dotnetru.app";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug("Push", "AuditUpdate. Push Notification receviced!");
+
+            Task updateAudit = UpdateService.UpdateAudit().ContinueWith(
+                t => this.SendNotification(Application.Context, "New information", "New DotNetRu information is available"));
+        }
+
+        private void SendNotification(Context context, string title, string body)
+        {
+            // Set up an intent so that tapping the notifications returns to this app:
+            Intent intent = new Intent(context, typeof(MainActivity));
+
+            // Create a PendingIntent; we're only using one PendingIntent (ID = 0):
+            const int PendingIntentID = 0;
+            PendingIntent pendingIntent = PendingIntent.GetActivity(
+                context,
+                PendingIntentID,
+                intent,
+                PendingIntentFlags.OneShot);
+
+            string channelName = "New data";
+            var importance = NotificationImportance.High;
+            NotificationChannel notificationChannel =
+                new NotificationChannel(AuditUpdateChannel, channelName, importance);
+
+            notificationChannel.EnableVibration(vibration: true);
+            notificationChannel.LockscreenVisibility = NotificationVisibility.Public;
+
+            Notification notification = new Notification.Builder(context)
+                .SetSmallIcon(Resource.Drawable.ic_launcher)
+                .SetContentTitle(title)
+                .SetContentText(body)
+                .SetContentIntent(pendingIntent)
+                .SetChannelId(AuditUpdateChannel)
+                .Build();
+
+            NotificationManager notificationManager =
+                (NotificationManager)context.GetSystemService(NotificationService);
+
+            notificationManager.CreateNotificationChannel(notificationChannel);
+
+            notificationManager.Notify(Settings.GetUniqueNotificationID(), notification);
+        }
+    }
+}
