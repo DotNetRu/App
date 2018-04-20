5388910e9853fdd857b65d4b3809f27fd3aae57e
Yury Belousov <yury-belousov@yandex.ru>
2018-04-20 03:58:22 +0300

    Initial support of sessions

---
+++ DotNetRu.DataStore.Audit/Models/SessionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNetRu.DataStore.Audit.Models
+{
+    using System;
+
+    public class SessionModel
+    {
+        public TalkModel Talk { get; set; }
+
+        public DateTimeOffset StartTime { get; set; }
+
+        public DateTimeOffset EndTime { get; set; }
+
+
+        public string Time => $"{this.StartTime.LocalDateTime.ToShortTimeString()} — {this.EndTime.LocalDateTime.ToShortTimeString()}";
+
+        // public MeetupModel Meetup { get; set; }
+    }
+}
