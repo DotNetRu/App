fb91c6885583ea5273b6eba42a74450ca453daf6
Pavel <pavel.fedotovsky@outlook.com>
2018-05-13 22:38:47 +0300

    Replaced listview with custom cells in Settings page

--- DotNetRu.Clients.Portable/Model/CommunityModel.cs
+++ DotNetRu.Clients.Portable/Model/CommunityModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace DotNetRu.Clients.Portable.Model
 {
+    using Xamarin.Forms;
+
     public class CommunityModel
     {
         public string VKLink { get; set; }
+
+        public string Name { get; set; }
+
+        public ImageSource ImageSource { get; set; }
     }
 }
