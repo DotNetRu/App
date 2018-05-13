17816482e7fc19af2954dc66064b012183794fd4
Pavel <pavel.fedotovsky@outlook.com>
2018-05-13 19:42:58 +0300

    Replacing ListView in Settings page with custom viewcells

---
+++ DotNetRu.Clients.Portable/Model/Communities.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetRu.Clients.Portable.Model
+{
+    using DotNetRu.Utils.Helpers;
+
+    public static class Communities
+    {
+        public static CommunityModel SaintPetersburg => new CommunityModel() { VKLink = AboutThisApp.SpbLink };
+    }
+}
