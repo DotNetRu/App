522a1caaa22cb7247ab67282d519e66c883d0a2f
Pavel <pavel.fedotovsky@outlook.com>
2018-05-13 22:38:47 +0300

    Replaced listview with custom cells in Settings page

--- DotNetRu.Clients.UI/Cells/CommunityViewCell.xaml.cs
+++ DotNetRu.Clients.UI/Cells/CommunityViewCell.xaml.cs
@@ -1,36 +1,23 @@
 ﻿namespace DotNetRu.Clients.UI.Cells
 {
-    using System.Windows.Input;
-
     using DotNetRu.Clients.Portable.Model;
 
     using Xamarin.Forms;
-    using Xamarin.Forms.Xaml;
 
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CommunityViewCell : ViewCell
+    public partial class CommunityViewCell
     {
-        public static readonly BindableProperty LaunchBrowserCommandProperty =
-            BindableProperty.Create(nameof(LaunchBrowserCommand), typeof(string), typeof(CommunityViewCell));
-
         public static readonly BindableProperty CommunityProperty =
-            BindableProperty.Create(nameof(Community), typeof(string), typeof(CommunityViewCell));
+            BindableProperty.Create(nameof(Community), typeof(CommunityModel), typeof(CommunityViewCell));
 
         public CommunityViewCell()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
         public CommunityModel Community
         {
             get => (CommunityModel)this.GetValue(CommunityProperty);
             set => this.SetValue(CommunityProperty, value);
-        }
-
-        public ICommand LaunchBrowserCommand
-        {
-            get => (ICommand)this.GetValue(LaunchBrowserCommandProperty);
-            set => this.SetValue(LaunchBrowserCommandProperty, value);
         }
     }
 }
