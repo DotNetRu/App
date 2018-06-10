03a2cede4841fa4130141732bd14ef9d38472f8e
Pavel <pavel.fedotovsky@outlook.com>
2018-06-11 00:17:17 +0300

    Removed hack realted to .NET standard as new version of Xamarin handles .NET Standard properly

--- DotNetRu.iOS/appcenter-pre-build.sh
+++
@@ -1,2 +0,0 @@
-﻿#!/usr/bin/env bash 
-cp -f -v /Users/vsts/.nuget/packages/system.runtime.compilerservices.unsafe/4.4.0/lib/netstandard2.0/System.Runtime.CompilerServices.Unsafe.dll /Users/vsts/.nuget/packages/system.runtime.compilerservices.unsafe/4.4.0/ref/netstandard2.0/System.Runtime.CompilerServices.Unsafe.dll
