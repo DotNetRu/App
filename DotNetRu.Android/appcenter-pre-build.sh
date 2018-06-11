708e3cd79597202503105dfc26874d92feb74356
Pavel <pavel.fedotovsky@outlook.com>
2018-06-11 21:55:34 +0300

    Attempt to fix pre-build script in AppCenter

--- DotNetRu.Android/appcenter-pre-build.sh
+++ DotNetRu.Android/appcenter-pre-build.sh
@@ -2,5 +2,5 @@
 
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
-  sed -i 's/6f9a7703-8ca4-477e-9558-7e095f7d20aa/79c348e1-2cfe-4ba6-b665-95d5fd6d5c80/' $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Clients.UI/App.xaml.cs
+  sed -i'' 's/6f9a7703-8ca4-477e-9558-7e095f7d20aa/79c348e1-2cfe-4ba6-b665-95d5fd6d5c80/' $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Clients.UI/App.xaml.cs
 fi
