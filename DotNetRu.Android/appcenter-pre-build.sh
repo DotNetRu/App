0fd7996f470097a5fd1ea10ab6b167d08ef314c7
Pavel <pavel.fedotovsky@outlook.com>
2018-06-12 12:16:37 +0300

    Changed pre-build script to only master branch & production config

--- DotNetRu.Android/appcenter-pre-build.sh
+++ DotNetRu.Android/appcenter-pre-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$APPCENTER_BRANCH" != "master" ];
+if [ "$APPCENTER_BRANCH" == "master" ];
 then
-  sed -i '' "s/6f9a7703-8ca4-477e-9558-7e095f7d20aa/79c348e1-2cfe-4ba6-b665-95d5fd6d5c80/" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Clients.UI/App.xaml.cs
+  sed -i '' "s/6f9a7703-8ca4-477e-9558-7e095f7d20aa/33fe8e32-1b4f-41a1-a835-88f345b31f98/" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Clients.UI/App.xaml.cs
 fi
