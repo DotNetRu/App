632ddf630183d2ea78ee0438ef0b573d64fa3076
Vadim Litvinov <vadlit@vadlit.ru>
2019-05-08 13:16:10 +0300

    234: fix audit updating when session has been changed

--- DotNetRu.DataStore.Audit/Services/UpdateService.cs
+++ DotNetRu.DataStore.Audit/Services/UpdateService.cs
@@ -148,7 +148,7 @@ namespace DotNetRu.DataStore.Audit.Services
                         cfg.CreateMap<MeetupEntity, Meetup>()
                             .ForMember(
                                 dest => dest.Sessions,
-                                o => o.MapFrom((src, dest, sessions, context) => context.Mapper.Map(src.Sessions, dest.Sessions)))
+                                o => o.MapFrom(src => src.Sessions))
                             .AfterMap(
                                 (src, dest) =>
                                     {
