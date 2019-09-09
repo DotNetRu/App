d63071fd654958d17110aeabdcef031d587a8aa3
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2019-09-09 23:52:45 +0300

    Fix issue with tweet function url in config

--- DotNetRu.Utils/Config/AppConfig.cs
+++ DotNetRu.Utils/Config/AppConfig.cs
@@ -10,7 +10,7 @@ namespace DotNetRu.Clients.UI
 
         public string AppCenteriOSKey { get; set; }
 
-        public string TweetFunctionURL { get; set; }
+        public string TweetFunctionUrl { get; set; }
         public string RealmDatabase { get; set; }
 
         public string RealmServerUrl { get; set; }
@@ -22,7 +22,7 @@ namespace DotNetRu.Clients.UI
             {
                 AppCenterAndroidKey = "6f9a7703-8ca4-477e-9558-7e095f7d20aa",
                 AppCenteriOSKey = "1e7f311f-1055-4ec9-8b00-0302015ab8ae",
-                TweetFunctionURL = "https://dotnettweetservice.azurewebsites.net/api/Tweets",
+                TweetFunctionUrl = "https://dotnettweetservice.azurewebsites.net/api/Tweets",
                 RealmDatabase = "dotnetru_050919",
                 RealmServerUrl = "dotnetru.de1a.cloud.realm.io"
         };
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,6 @@
 
 sed -i '' "s/APP_CENTER_ANDROID_KEY/$AppCenterAndroidKey/" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Utils/Config/config.json
 sed -i '' "s/APP_CENTER_IOS_KEY/$AppCenteriOSKey/" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Utils/Config/config.json
-sed -i '' "s%TWEET_FUNCTION_URL%$TweetFunctionURLKey%" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Utils/Config/config.json
+sed -i '' "s%TWEET_FUNCTION_URL%$TweetFunctionUrl%" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Utils/Config/config.json
 sed -i '' "s%REALM_DATABASE%$RealmDatabase%" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Utils/Config/config.json
 sed -i '' "s%REALM_SERVER_URL%$RealmServerUrl%" $APPCENTER_SOURCE_DIRECTORY/DotNetRu.Utils/Config/config.json
