985e9ca6915b3dcfe292ee08f628a701df6fedb9
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2019-12-28 22:47:20 +0300

    Fix reading configuration

--- DotNetRu.Utils/Config/AppConfig.cs
+++ DotNetRu.Utils/Config/AppConfig.cs
@@ -18,7 +18,7 @@ namespace DotNetRu.Clients.UI
 
         public static AppConfig GetConfig()
         {
-            var configBytes = ResourceHelper.ExtractResource("DotNetRu.Utils.Config.config.json");
+            var configBytes = ResourceHelper.ExtractResource("config.json");
             var configBytesAsString = Encoding.UTF8.GetString(configBytes);
             return JsonConvert.DeserializeObject<AppConfig>(configBytesAsString);
         }
