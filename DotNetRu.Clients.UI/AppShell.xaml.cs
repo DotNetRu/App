36ce530ca244df5b24e443cc506f147a73f0846a
Pavel Fedotovsky <pavel.fedotovsky@outlook.com>
2020-02-01 00:28:44 +0300

    Update to AppShell

---
+++ DotNetRu.Clients.UI/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+using Xamarin.Forms;
+
+namespace DotNetRu.Clients.UI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            var primaryColor = (Color)Application.Current.Resources["Primary"];
+
+            var backgroundColor = Device.RuntimePlatform == Device.iOS ? Color.White : primaryColor;
+
+            // Can't set BackgrounColor as StaticResource, see https://github.com/xamarin/Xamarin.Forms/issues/7055
+            SetBackgroundColor(this, backgroundColor);
+        }
+    }
+}
