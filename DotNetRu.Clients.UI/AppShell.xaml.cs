35f61826895cd36f6dbf47599c5a5a96ef561e7c
Chebotov Nikolay <spiritkola@hotmail.com>
2020-03-24 17:19:19 +0300

    Add fix: change TabBar localization by settings

--- DotNetRu.Clients.UI/AppShell.xaml.cs
+++ DotNetRu.Clients.UI/AppShell.xaml.cs
@@ -1,3 +1,6 @@
+using DotNetRu.Clients.Portable.Helpers;
+using DotNetRu.Clients.Portable.ViewModel;
+using DotNetRu.Utils.Helpers;
 using Xamarin.Forms;
 
 namespace DotNetRu.Clients.UI
@@ -15,6 +18,22 @@ namespace DotNetRu.Clients.UI
 
             // Can't set BackgrounColor as StaticResource, see https://github.com/xamarin/Xamarin.Forms/issues/7055
             SetBackgroundColor(this, backgroundColor);
+
+            MessagingCenter.Subscribe<LocalizedResources>(this, MessageKeys.LanguageChanged, sender => this.UpdateTabBarLocalization());
+        }
+
+        public void UpdateTabBarLocalization()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                if (this.BindingContext is ViewModelBase viewModel)
+                {
+                    this.NewsTab.Title = viewModel.Resources["News"] ?? this.NewsTab.Title;
+                    this.SpeakersTab.Title = viewModel.Resources["Speakers"] ?? this.SpeakersTab.Title;
+                    this.MeetupsTab.Title = viewModel.Resources["Meetups"] ?? this.MeetupsTab.Title;
+                    this.AboutTab.Title = viewModel.Resources["About"] ?? this.AboutTab.Title;
+                }
+            });
         }
     }
 }
