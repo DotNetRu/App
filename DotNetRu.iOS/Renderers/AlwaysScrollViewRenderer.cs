dac41302c397f5f56af72a713e211a3a55315674
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-10 22:28:45 +0400

    Update iOS project to new csproj and MAUI

--- DotNetRu.iOS/Renderers/AlwaysScrollViewRenderer.cs
+++ DotNetRu.iOS/Renderers/AlwaysScrollViewRenderer.cs
@@ -1,24 +1,23 @@
-﻿using DotNetRu.Clients.UI.Controls;
-using DotNetRu.iOS.Renderers;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
+﻿//using DotNetRu.Clients.UI.Controls;
+//using DotNetRu.iOS.Renderers;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(NonScrollableListView), typeof(NonScrollableListViewRenderer))]
-[assembly: ExportRenderer(typeof(AlwaysScrollView), typeof(AlwaysScrollViewRenderer))]
+//[assembly: ExportRenderer(typeof(NonScrollableListView), typeof(NonScrollableListViewRenderer))]
+//[assembly: ExportRenderer(typeof(AlwaysScrollView), typeof(AlwaysScrollViewRenderer))]
 
-namespace DotNetRu.iOS.Renderers
-{
-    public class AlwaysScrollViewRenderer : ScrollViewRenderer
-    {
-        public static void Initialize()
-        {
-        }
+//namespace DotNetRu.iOS.Renderers
+//{
+//    public class AlwaysScrollViewRenderer : ScrollViewRenderer
+//    {
+//        public static void Initialize()
+//        {
+//        }
 
-        protected override void OnElementChanged(VisualElementChangedEventArgs e)
-        {
-            base.OnElementChanged(e);
-            this.AlwaysBounceVertical = true;
-        }
-    }
-}
-
+//        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+//        {
+//            base.OnElementChanged(e);
+//            this.AlwaysBounceVertical = true;
+//        }
+//    }
+//}
--- DotNetRu.iOS/Renderers/NonScrollableListViewRenderer.cs
+++ DotNetRu.iOS/Renderers/NonScrollableListViewRenderer.cs
@@ -1,28 +1,28 @@
-using DotNetRu.Clients.UI.Controls;
-using DotNetRu.iOS.Renderers;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
+//using DotNetRu.Clients.UI.Controls;
+//using DotNetRu.iOS.Renderers;
+//using Xamarin.Forms;
+//using Xamarin.Forms.Platform.iOS;
 
-[assembly: ExportRenderer(typeof(NonScrollableListView), typeof(NonScrollableListViewRenderer))]
-[assembly: ExportRenderer(typeof(AlwaysScrollView), typeof(AlwaysScrollViewRenderer))]
+//[assembly: ExportRenderer(typeof(NonScrollableListView), typeof(NonScrollableListViewRenderer))]
+//[assembly: ExportRenderer(typeof(AlwaysScrollView), typeof(AlwaysScrollViewRenderer))]
 
-namespace DotNetRu.iOS.Renderers
-{
-    public class NonScrollableListViewRenderer : ListViewRenderer
-    {
-        public static void Initialize()
-        {
-        }
+//namespace DotNetRu.iOS.Renderers
+//{
+//    public class NonScrollableListViewRenderer : ListViewRenderer
+//    {
+//        public static void Initialize()
+//        {
+//        }
 
-        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
-        {
-            base.OnElementChanged(e);
-            if (this.Control != null)
-            {
-                this.Control.ScrollEnabled = false;
-                this.Control.ShowsVerticalScrollIndicator = false;
-            }
-        }
-    }
-}
+//        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+//        {
+//            base.OnElementChanged(e);
+//            if (this.Control != null)
+//            {
+//                this.Control.ScrollEnabled = false;
+//                this.Control.ShowsVerticalScrollIndicator = false;
+//            }
+//        }
+//    }
+//}
 
