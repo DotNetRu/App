81d4d96755194d11675a3f9c10cc90ab38d0262e
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-12 01:21:37 +0400

    Add support for C# 10 in AppCenter (attempt 3)

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -5,4 +5,4 @@ echo "Pre-build script executing..."
 cp $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.$Environment.json $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.json
 
 # Use msbuild from Visual Studio
-sudo sed -i '' 's#/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/mono/msbuild/15.0/bin/MSBuild.dll#/Applications/Visual\ Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll#' /Library/Frameworks/Mono.framework/Commands/msbuild
+sudo sed -i '' 's#/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/mono/msbuild/15.0/bin/MSBuild.dll#/Applications/Visual\\ Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll#' /Library/Frameworks/Mono.framework/Commands/msbuild
