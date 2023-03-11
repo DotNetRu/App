ee4751b5da5da713753701587c83a17a8a798ec6
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-12 01:13:06 +0400

    Add support for C# 10 in AppCenter (attempt 2)

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,8 +4,5 @@ echo "Pre-build script executing..."
 
 cp $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.$Environment.json $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.json
 
-# Add support for C# 10
-MonoFrameworkPackage=MonoFramework-MDK-6.12.0.182.macos10.xamarin.universal.pkg
-wget https://download.mono-project.com/archive/6.12.0/macos-10-universal/$MonoFrameworkPackage
-sudo chmod +x $MonoFrameworkPackage
-sudo installer -pkg $MonoFrameworkPackage -target /
+# Use msbuild from Visual Studio
+sudo sed -i '' 's#/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/mono/msbuild/15.0/bin/MSBuild.dll#/Applications/Visual\ Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll#' /Library/Frameworks/Mono.framework/Commands/msbuild
