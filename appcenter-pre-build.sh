c1a060f0707e52839d8815caf511e33739b7b252
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-12 12:24:44 +0400

    Improve readability of the pre-build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -4,5 +4,8 @@ echo "Pre-build script executing..."
 
 cp $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.$Environment.json $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.json
 
+msBuildMonoPath='/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/mono/msbuild/15.0/bin/MSBuild.dll'
+msBuildVSforMacPath='/Applications/Visual\\ Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll'
+
 # Use msbuild from Visual Studio
-sudo sed -i '' 's#/Library/Frameworks/Mono.framework/Versions/6.12.0/lib/mono/msbuild/15.0/bin/MSBuild.dll#/Applications/Visual\\ Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll#' /Library/Frameworks/Mono.framework/Commands/msbuild
+sudo sed -i '' "s#$msBuildMonoPath#$msBuildVSforMacPath#" /Library/Frameworks/Mono.framework/Commands/msbuild
