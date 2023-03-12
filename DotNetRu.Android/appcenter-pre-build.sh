3142f368fb52c303f7a4b7e1835c89f227f4a708
pfedotovsky <pavel.fedotovsky@outlook.com>
2023-03-12 13:08:27 +0400

    Change Android pre-build to use global pre-build script

--- DotNetRu.Android/appcenter-pre-build.sh
+++ DotNetRu.Android/appcenter-pre-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
+echo "Call global pre-build script"
 
-echo "Pre-build script executing..."
-
-cp $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.$Environment.json $APPCENTER_SOURCE_DIRECTORY/DotNetRu.AppUtils/Config/config.json
+source ../appcenter-pre-build.sh
